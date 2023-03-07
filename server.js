122438e778dc2d6f16209cc958afe2bedab9af5c
Erick H <erick.souza@grupoautomax.com.br>
2023-03-07 14:45:18 -0300

    edit contrato

--- server.js
+++ server.js
@@ -22,6 +22,53 @@ app.get('/', (req, res) => {
   res.render('form');
 });
 
+app.get('/main', (req, res) => {
+  res.render('main');
+});
+
+app.post('/contrato', (req, res) => {
+  const vendedor = req.body.vendedor;
+  const nome = req.body.nome;
+  const endereco = req.body.endereco;
+  const dataNascimento = req.body.dataNascimento;
+  const modeloVeiculo = req.body.modeloVeiculo;
+
+  
+  res.render('contrato', { nome, endereco, dataNascimento, modeloVeiculo});
+});
+
+app.post('/viewContratoVN',(req, res) => {
+    const vendedor = req.body.vendedor;
+    const nomeCliente = req.body.nomeCliente;
+    const dataNascimento = req.body.dataNascimento;
+    const RG = req.body.rg;
+    const CPF_CNPJ = req.body.CPF_CNPJ;
+    const telefone = req.body.telefone;
+    const cep = req.body.cep;
+    const numero = req.body.numero;
+    const bairro = req.body.bairro;
+    const complemento = req.body.complemento;
+    const cidade = req.body.cidade;
+    const estado = req.body.estado;
+
+    const modeloVeiculo = req.body.modeloVeiculo;
+    const corVeiculo = req.body.corVeiculo;
+    const opcionais = req.body.opcionais;
+    const cortesia = req.body.cortesia;
+    const obs = req.body.obs;
+    const prazoEntrega = req.body.prazoEntrega;
+    const valorVeiculo = req.body.valorVeiculo;
+    const sinal = req.body.sinal;
+    const valorTributos = req.body.valorTributos;
+    const veiculoSeminovo = req.body.veiculoSeminovo;
+    const avaliacao = req.body.avaliacao;
+    const loja = req.body.loja;
+
+  res.render('viewContratoVN',  { vendedor, nomeCliente, endereco, dataNascimento, RG, CPF_CNPJ, telefone, cep,
+    numero, bairro, complemento, cidade, estado, modeloVeiculo, corVeiculo, opcionais, 
+    cortesia, obs, prazoEntrega, valorVeiculo, sinal, valorTributos, veiculoSeminovo, avaliacao, loja });
+})
+
 app.post('/resultContratoVN', (req, res) => {
     const vendedor = req.body.vendedor;
     const nomeCliente = req.body.nomeCliente;
