4fa21a4a8cfa60c10429765dd89c45ece7148708
Erick H <erick.souza@grupoautomax.com.br>
2023-03-23 17:11:45 -0300

    config controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) =>{
-    res.render('login');
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
 //cadastro login
@@ -9,7 +10,7 @@ exports.register = async function(req, res){
     try {
         const login = new Login(req.body);
         await login.register();
-    
+
         if(login.errors.length > 0){
             req.flash('erros', login.errors);
             req.session.save(function(){
@@ -24,9 +25,38 @@ exports.register = async function(req, res){
         });
 
         return res.send(login.errors);
-        
+
     } catch (error) {
         console.log(e);
         return res.render('404');
-    } 
+    }
+};
+
+exports.login = async function(req, res){
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0){
+      req.flash('errors', login.errors);
+      req.session.save(function(){
+        return res.redirect('back');
+      });
+      return;
+    }
+
+    req.flash('sucesso', 'Você logou com sucesso!');
+    req.session.user = login.user;
+    req.session.savae(function() {
+      return res.redirect('back');
+    });
+  } catch(e){
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function(req, res ){
+  req.session.destroy();
+  res.redirect('/');
 };
